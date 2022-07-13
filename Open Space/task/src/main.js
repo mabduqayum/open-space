7f4255099338e0c2369ff253f72eea21789dd728
mAbduqayum <mabduqayum@gmail.com>
2022-07-13 14:02:59 +0500

    finished last task.

--- Open Space/task/src/main.js
+++ Open Space/task/src/main.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const password = document.getElementById('password-field');
-const checkButtons = document.getElementsByClassName('check-buttons')[0];
-const leverButtons = document.getElementsByClassName('levers')[0];
+const checkButtons = document.querySelectorAll('.check-buttons > input');
+const leverButtons = document.querySelectorAll('.levers > input');
 const launchButton = document.getElementsByClassName('launch-rocket')[0];
-
+const rocket = document.getElementById('rocket');
 
 function onSubmitPassword(self, event) {
     const passwordValue = password.value;
@@ -13,9 +13,37 @@ function onSubmitPassword(self, event) {
     if (passwordValue !== 'TrustNo1') {
         return;
     }
-    const items = [...checkButtons.children, ...leverButtons.children, launchButton, password, self];
+    const items = [...checkButtons, ...leverButtons, password, self];
     for (const item of items) {
         item.toggleAttribute('disabled')
     }
 }
 
+
+function getLaunchStatus() {
+    let launchStatus = true;
+    for (const checkButton of checkButtons) {
+        if (!checkButton.checked) {
+            launchStatus = false;
+            break;
+        }
+    }
+    for (const leverButton of leverButtons) {
+        if (leverButton.value < 100) {
+            launchStatus = false;
+            break;
+        }
+    }
+    if (launchStatus) {
+        launchButton.removeAttribute('disabled');
+    } else {
+        launchButton.setAttribute('disabled', '');
+    }
+}
+
+launchButton.addEventListener('click', event => {
+    let rocketBottom = rocket.clientTop + rocket.clientHeight;
+    let rocketLeft = rocket.offsetLeft;
+    rocket.style.bottom = rocketBottom + 1500 + 'px';
+    rocket.style.left = ++rocketLeft + 1500 + 'px';
+})
